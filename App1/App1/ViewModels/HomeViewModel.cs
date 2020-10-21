bbf1ac783b2fa823a74e64572eefc293a3d16775
Grady Wills <n10341251@qut.edu.au>
2020-10-21 16:14:30 +1000

    Add project files.

---
+++ App1/App1/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace App1.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
---
+++ App1/App1/Views/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
